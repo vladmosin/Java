3263621563af4c3802cd8d9a813d274e0321fb8d
Vladislav Mosin <vmos1999@mail.ru>
2019-01-24 23:26:07 +0300

    Function, which adds elements to to Trie implemented
    Also some auxiliary functions in class TrieNode, which helps adding element to Trie, were implemented

--- Trie/src/main/java/com/hse/trie/Trie.java
+++ Trie/src/main/java/com/hse/trie/Trie.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class Trie {
     private static class TrieNode {
-        private HashMap<Character, TreeNode> nextTreeNode;
+        private HashMap<Character, TrieNode> nextTreeNode;
         private int suffixNumber;
         private boolean isEndOfWord;
 
@@ -13,5 +13,51 @@ public class Trie {
             this.isEndOfWord = isEndOfWord;
             suffixNumber = 1;
         }
+
+        public TrieNode() {
+        }
+
+        public boolean containsNext(char symbol) {
+            return nextTreeNode.containsKey(symbol);
+        }
+
+        public TrieNode getNext(char symbol) {
+            return nextTreeNode.get(symbol);
+        }
+
+        public void addNext(char symbol, TrieNode trieNode) throws IllegalArgumentException {
+            if (trieNode == null) {
+                throw new IllegalArgumentException("Trie node is null");
+            }
+
+            nextTreeNode.put(symbol, trieNode);
+        }
+    }
+
+    private TrieNode root;
+    private int size;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public boolean add(String string) {
+        TrieNode currentNode = root;
+        boolean isNew = false;
+
+        for (int index = 0; index < string.length(); index++) {
+            if (!currentNode.containsNext(string.charAt(index))) {
+                isNew = true;
+                if (index == string.length() - 1) {
+                    currentNode.addNext(string.charAt(index), new TrieNode(true));
+                } else {
+                    currentNode.addNext(string.charAt(index), new TrieNode(false));
+                }
+            }
+
+            currentNode = currentNode.getNext(string.charAt(index));
+        }
+
+        return isNew;
     }
 }
