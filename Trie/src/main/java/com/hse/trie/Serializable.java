082594130ac72de70006aeede7f96e95081ce7aa
Vladislav Mosin <vmos1999@mail.ru>
2019-01-26 13:52:30 +0300

    Added JavaDoc to Serializable

--- Trie/src/main/java/com/hse/trie/Serializable.java
+++ Trie/src/main/java/com/hse/trie/Serializable.java
@@ -4,8 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * Interface to convert byte sequence to object and conversely
+ * */
 public interface Serializable {
+    /**
+     * Converts object to byte sequence in OutputStream
+     * */
     void serialize(OutputStream out) throws IOException;
 
+    /**
+     * Creates object from data in InputStream and replace current object with it
+     * */
     void deserialize(InputStream in) throws IOException;
 }
