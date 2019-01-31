977376c0ee96b009f912f06511c7d77d19f46d63
Vladislav Mosin <vmos1999@mail.ru>
2019-01-31 23:20:40 +0300

    BinarySearchTreeIterator implemented

--- BinarySearchTree/src/main/java/com/hse/binarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/src/main/java/com/hse/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.hse.binarySearchTree;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.AbstractSet;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -20,45 +22,125 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends AbstractS
         }
     }
 
+    public class BinarySearchTreeIterator implements Iterator<E> {
+        Node<E> node;
+        private boolean isReversed;
+
+        public BinarySearchTreeIterator(@NotNull Node<E> node, boolean isReversed) {
+            this.node = node;
+            this.isReversed = isReversed;
+        }
+
+        public boolean hasPrevious() {
+            if (isReversed) {
+                return findNext() != null;
+            }
+
+            return findPrevious() != null;
+        }
+
+        @NotNull public E previous() {
+            if (!hasPrevious()) {
+                throw new IllegalArgumentException("no previous element");
+            }
+
+            E element = null;
+
+            if (isReversed) {
+                node = findNext();
+                element = node.value;
+            } else {
+                element = node.value;
+                node = findPrevious();
+            }
+
+            return element;
+        }
+
+        public boolean hasNext() {
+            if (isReversed) {
+                return findPrevious() != null;
+            }
+
+            return findNext() != null;
+        }
+
+        @NotNull public E next() {
+            if (!hasNext()) {
+                throw new IllegalArgumentException("no next element");
+            }
+
+            E element = null;
+
+            if (isReversed) {
+                element = node.value;
+                node = findPrevious();
+            } else {
+                node = findNext();
+                element = node.value;
+            }
+
+            return element;
+        }
+
+        private Node<E> findNext() {
+            return node.findNext();
+        }
+
+        private Node<E> findPrevious() {
+            return node.findPrevious();
+        }
+    }
+
     private boolean isReversed;
     private Comparator<? super E> comparator;
 
+    @Override
     public Iterator descendingIterator() {
         return null;
     }
 
+    @Override
     public MyTreeSet descendingSet() {
         return null;
     }
 
+    @Override
     public E first() {
         return null;
     }
 
+    @Override
     public E last() {
         return null;
     }
 
+    @Override
     public E lower(Object o) {
         return null;
     }
 
+    @Override
     public E floor(Object o) {
         return null;
     }
 
+    @Override
     public E ceiling(Object o) {
         return null;
     }
 
+    @Override
     public E higher(Object o) {
         return null;
     }
 
+    @Override
     public Iterator iterator() {
         return null;
     }
 
+    @Override
     public int size() {
         return 0;
     }
